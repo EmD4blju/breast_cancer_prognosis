2d8f34bd1870c17ba07d13a7623296f9fa0110e8
EmD4blju <mikolaj.warda@gmail.com>
2024-12-20 12:57:12 +0100

    Setup data preparator

---
+++ src/data_preprocessing/data_preparator.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def load(path:str) -> pd.DataFrame:
+    pass
+
+def encode(dataframe:pd.DataFrame, columns_to_encode:list) -> pd.DataFrame:
+    pass
+
+def replace_nan(dataframe:pd.DataFrame, to_replace:str, value, columns:list) -> pd.DataFrame:
+    pass
+
+def separate(dataframe:pd.DataFrame, id_label:str, outcome_label:str, feature_start_label:int, after:bool):
+    pass
+
+def prepare_wbpc(dataframe:pd.DataFrame):
+    pass
